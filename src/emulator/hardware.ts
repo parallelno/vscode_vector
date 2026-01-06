8a6aa89b01a797fd60aa130592c344570be67dfa
parallelno <mailforfriendparallelno@gmail.com>
2026-01-06 08:47:03 -0800

    Improved rom hot reload - The system automatically adjusts the PC reg to maintain execution flow: it captures the PC and nearby labels (within 100 bytes) before recompilation, then after recompilation, it matches label addresses and adjusts the PC by the same offset to preserve the execution position. This helps prevent disruption when code before the execution point changes size

--- src/emulator/hardware.ts
+++ src/emulator/hardware.ts
@@ -190,11 +190,13 @@ export class Hardware
     case HardwareReq.GET_REG_PC:
       out = {"pc": this._cpu?.pc };
       break;
-    
+
     case HardwareReq.SET_REG_PC:
-      if (this._cpu && data && typeof data["pc"] === "number") {
-        this._cpu.state.regs.pc.word = data["pc"] & 0xffff;
+      const pc: number = data["pc"];
+      if (pc === undefined) {
+        break;
       }
+      this._cpu.state.regs.pc.word = pc;
       break;
 /*
     case HardwareReq.GET_RUSLAT_HISTORY:
